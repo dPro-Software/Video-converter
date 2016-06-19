c6178c7072614d688c62453111a83be835f1aaf5
Damiaan Dufaux <info@devian.tk>
2016-06-19 16:13:50 +0200

    Initial Commit

---
+++ Video converter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Video converter
+//
+//  Created by Damiaan Dufaux on 19/06/16.
+//  Copyright © 2016 Damiaan Dufaux. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
