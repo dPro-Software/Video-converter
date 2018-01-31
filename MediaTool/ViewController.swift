09cb5506379b71469a3caf0767c857c265d7884f
Damiaan Dufaux <damiaan@dpro.be>
2018-01-31 16:50:40 +0100

    Initial Commit

---
+++ MediaTool/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MediaTool
+//
+//  Created by Damiaan on 31/01/18.
+//  Copyright © 2018 Devian. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
